3333af1553fd3c29518533565e88ea556644908e
vsobol <viktor.sobol@agoda.com>
2023-08-18 18:30:48 +0700

    first commit

---
+++ .zprofile
@@ -0,0 +1,9 @@
+source ~/.profile
+# Add Visual Studio Code (code)
+export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+
+
+# Added by Toolbox App
+export PATH="$PATH:/usr/local/bin"
+
+source ~/dotfiles/.all_files
---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+
+
+ln -s ~/dotfiles/.aliases ~/.aliases
+ln -s ~/dotfiles/.functions ~/.functions
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.zprofile ~/.zprofile
+ln -s ~/dotfiles/.env ~/.env
